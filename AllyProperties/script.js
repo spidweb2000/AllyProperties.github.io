7ccf8374b2502828114c74869bd7b8d2f4f02666
spidweb2000 <117068977+spidweb2000@users.noreply.github.com>
2022-11-22 23:29:56 +0800

    after second meeting 1

--- AllyProperties/script.js
+++ AllyProperties/script.js
@@ -31,3 +31,27 @@ function closeNav() {
     document.getElementById("mySidepanelM").style.width = "0";
     document.getElementById("mySidepanelS").style.width = "0";
 }
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
